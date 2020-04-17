177314b1102e9a2e72fcce7aac7d8ef344f37216
esoft45 <paulo.kolbe@gmail.com>
2020-04-17 16:30:25 -0300

    initial setup

---
+++ src/main/java/gft/com/tddb3/TddB3Application.java
@@ -0,0 +1,13 @@
+package gft.com.tddb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddB3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddB3Application.class, args);
+	}
+
+}
---
+++ src/test/java/gft/com/tddb3/TddB3ApplicationTests.java
@@ -0,0 +1,13 @@
+package gft.com.tddb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddB3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
