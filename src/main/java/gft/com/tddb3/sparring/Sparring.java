9e44c35043cb0f894fc5428e6e4930a0122826a5
esoft45 <paulo.kolbe@gmail.com>
2020-04-17 19:05:58 -0300

    first unit test

---
+++ src/main/java/gft/com/tddb3/sparring/Sparring.java
@@ -0,0 +1,28 @@
+package gft.com.tddb3.sparring;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded=true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Sparring {
+	
+	@Getter
+	private String nome;
+	
+	@Getter
+	private Integer xp;
+	
+	
+	private String[] golpes;
+	
+
+	public void recebeGolpes() {
+		this.xp -= 3;
+	}
+}
---
+++ src/test/java/gft/com/tddb3/sparring/SparringTest.java
@@ -0,0 +1,42 @@
+package gft.com.tddb3.sparring;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SparringTest {
+
+	static Sparring sparring;
+	
+	@BeforeAll
+	public static void setUpBeforeClass() throws Exception {
+		sparring = Sparring.builder().nome("Spar")
+				                     .xp(50)
+				                     .build();
+	}
+
+	@BeforeEach
+	public void setUp() throws Exception {
+	}	
+	
+	@Test
+	public void whenRecebeGolpes_thenDiminuiXp() {
+		int xp = sparring.getXp();
+
+		sparring.recebeGolpes();
+		
+		Assertions.assertTrue(sparring.getXp() < xp);
+	}
+	
+	@AfterAll
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+	}	
+
+}
